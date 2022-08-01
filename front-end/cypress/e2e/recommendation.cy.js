51a60ed8e0cf04e7fd5bea48586f9f7e465afc56
Ana Raquel Matos <anaraquelpmatos@gmail.com>
2022-08-01 13:24:12 -0300

    add: valid upvote cypress test

--- front-end/cypress/e2e/recommendation.cy.js
+++ front-end/cypress/e2e/recommendation.cy.js
@@ -43,9 +43,27 @@ describe("recommendation suit test", () => {
     cy.get("button").click();
     cy.wait("@createRecommendation");
 
-    cy.on('window:alert',(t)=>{
-      expect(t).to.contains('Error creating recommendation!');
-   })
+    cy.on("window:alert", (t) => {
+      expect(t).to.contains("Error creating recommendation!");
+    });
+    cy.url().should("equal", "http://localhost:3000/");
+  });
+
+  it("should insert upvote", () => {
+    const youtubeLink = `https://www.youtube.com/${faker.internet.password()}`;
+    const recommendation = {
+      name: faker.name.findName(),
+      youtubeLink: youtubeLink,
+    };
+    cy.visit("http://localhost:3000/");
+    cy.createRecommendation(recommendation);
+    cy.get("div:last-child article:last-child svg:first-child").click();
+    cy.intercept("POST", "/recommendations/1/upvote").as(
+      "upvote"
+    );
+    cy.get("button").click();
     cy.url().should("equal", "http://localhost:3000/");
   });
 });
+
+
--- front-end/cypress/support/commands.js
+++ front-end/cypress/support/commands.js
@@ -1,3 +1,7 @@
 Cypress.Commands.add("resetDatabase", () => {
     cy.request("POST", "http://localhost:5000/recommendations/reset");
 })
+
+Cypress.Commands.add("createRecommendation", (recommendation) => {
+    cy.request("POST", "http://localhost:5000/recommendations", recommendation);
+});
