75665340e4827501270cfbfcb52e068904a7b195
Ana Raquel Matos <anaraquelpmatos@gmail.com>
2022-08-01 12:51:38 -0300

    add: cypress setup

--- front-end/cypress/e2e/recommendation.cy.js
+++ front-end/cypress/e2e/recommendation.cy.js
@@ -1,28 +1,48 @@
 /// <reference types="cypress" />
 import { faker } from "@faker-js/faker";
 
-// beforeEach(() => {
-//     cy.resetDatabase()
-//   });
+beforeEach(() => {
+  cy.resetDatabase();
+});
 
 describe("recommendation suit test", () => {
+  it("should create recommendation", () => {
+    const youtubeLink = `https://www.youtube.com/${faker.internet.password()}`;
+    const recommendation = {
+      name: faker.name.findName(),
+      youtubeLink: youtubeLink,
+    };
 
-    it("should create recommendation", () => {
-        const youtubeLink = `https://www.youtube.com/${faker.internet.password()}`;
-        const recommendation = {
-            name: faker.name.findName(),
-            youtubeLink: youtubeLink
-        }
+    cy.visit("http://localhost:3000/");
+    cy.get('input[placeholder="Name"]').type(recommendation.name);
+    cy.get('input[placeholder="https://youtu.be/..."]').type(
+      recommendation.youtubeLink
+    );
 
-        cy.visit("http://localhost:3000/");
-        cy.get('input[placeholder="Name"]').type(recommendation.name);
-        cy.get('input[placeholder="https://youtu.be/..."]').type(recommendation.youtubeLink);
+    cy.intercept("POST", "/recommendations").as("createRecommendation");
+    cy.get("button").click();
+    cy.wait("@createRecommendation");
 
-        cy.intercept("POST", "/recommendations").as("createRecommendation");
-        cy.get("button").click();
-        cy.wait("@createRecommendation");
+    cy.url().should("equal", "http://localhost:3000/");
+  });
 
-        cy.url().should("equal", "http://localhost:3000/");
-    })
+  it("should create recommendation with invalid youtube link", () => {
+    const youtubeLink = `${faker.internet.password()}`;
+    const recommendation = {
+      name: faker.name.findName(),
+      youtubeLink: youtubeLink,
+    };
 
-})
+    cy.visit("http://localhost:3000/");
+    cy.get('input[placeholder="Name"]').type(recommendation.name);
+    cy.get('input[placeholder="https://youtu.be/..."]').type(
+      recommendation.youtubeLink
+    );
+
+    cy.intercept("POST", "/recommendations").as("createRecommendation");
+    cy.get("button").click();
+    cy.wait("@createRecommendation");
+
+    cy.url().should("equal", "http://localhost:3000/");
+  });
+});
